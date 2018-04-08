4d24c921b98d5c8d0ef87ec1eb9c12fbcedf1494
Makoto Tanabe <xekid78@outlook.com>
2018-04-08 11:35:40 +0900

    Add files via upload

---
+++ Method/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Method
+{
+    class Method
+    {
+        static void SumGokei()
+        {
+            int sum = 5 * 8;
+            Console.WriteLine(sum);
+        }
+
+        static void Main(string[] args)
+        {
+            SumGokei();
+        }
+    }
+}
